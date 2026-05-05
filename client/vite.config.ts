44bc68b8c4050c77b23fb176d51a95f6766cdb05
kabin-ux <dongolkabin@gmail.com>
2026-05-05 14:27:21 +0545

    url fix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5005',
+        target: 'https://survey-builder-app.onrender.com',
         changeOrigin: true
       }
     }
